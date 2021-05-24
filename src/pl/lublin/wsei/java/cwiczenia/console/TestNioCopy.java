e999a9817ffbee096db0e4179d8dfc000687f13e
Sebastian <HalfDead13@wp.pl>
2021-05-24 17:46:09 +0200

    9

--- src/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
+++ src/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
@@ -1,5 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia.console;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,9 +11,9 @@ public class TestNioCopy {
     public static void main (String[] args) throws IOException {
         //long startTime = System.currentTimeMillis();
         long startTime = System.nanoTime();
-        Path oryginal = Paths.get("sample_1920×1280.tiff");
-        Path kopia = Paths.get("img.copy2.tiff");
-        Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
+        File oryginal = new File("sample_1920×1280.tiff");
+        File kopia = new File("img.copy2.tiff");
+        Files.copy(oryginal.toPath(), kopia.toPath());
         //System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime)+" ms");
         System.out.println("Czas kopiowania: " + (float)(System.nanoTime() - startTime)/1000000+" ms");
 
