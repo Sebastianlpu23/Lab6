e25ea91941161a15a8bd3212cf7fcb1517dc1021
Sebastian <HalfDead13@wp.pl>
2021-05-24 18:43:04 +0200

    12

---
+++ src/pl/lublin/wsei/java/cwiczenia/console/TestCharacterCopyFlawed.java
@@ -0,0 +1,40 @@
+
+package pl.lublin.wsei.java.cwiczenia.console;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.time.Instant;
+
+public class TestCharacterCopyFlawed {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = null;
+        BufferedWriter out = null;
+
+        long startTime = Instant.now().toEpochMilli();
+
+        try{
+            in = new BufferedReader(new FileReader("sample_1920×1280.tiff"));
+            out = new BufferedWriter(new FileWriter("img_copy.tiff"));
+
+            int c;
+            while ((c = in.read()) != -1){
+                out.write(c);
+            }
+        }
+        catch (IOException e){
+            System.out.println("IOException: " + e.getMessage());
+            e.printStackTrace();
+        }
+        finally{
+            if (in != null){
+                in.close();
+            }
+            if (out != null){
+                out.close();
+            }
+        }
+
+        System.out.println("Czas wykonywania w nanosekundach: " + (Instant.now().toEpochMilli() - startTime));
+        System.out.println("Domyslne kodowanie: " + Charset.defaultCharset().displayName());
+    }
+}
